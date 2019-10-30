58ebf735ccff24687aa10d8363fa92d90c32d031
Quentin Meeus <qmeeus@gmail.com>
2019-10-30 11:13:03 +0100

    add .env file to git

--- api/config.py
+++ api/config.py
@@ -1,12 +1,12 @@
 import os
+import uuid
 
 
 class Development:
     DEBUG = True
-    SECRET_KEY = os.environ["SECRET_KEY"]
+    SECRET_KEY = os.environ.get("SECRET_KEY", uuid.uuid4().hex)
 
 
 class Production(Development):
 
     DEBUG = False
-
